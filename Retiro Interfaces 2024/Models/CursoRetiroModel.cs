04e5a40c9531dd14bd3c0ceb77d115af003e6605
DiegoDev <DiegoM_08.L@outlook.es>
2025-02-05 19:55:34 -0500

    Mejoras que no se GO

---
+++ Retiro Interfaces 2024/Models/CursoRetiroModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Retiro_Interfaces_2024.Models
+{
+    public class CursoRetiroModel
+    {
+        private string codigo;
+        private string nombre;
+        private string creditos;
+        private string docente;
+
+        public CursoRetiroModel(string codigo, string nombre, string creditos, string docente)
+        {
+            this.codigo = codigo;
+            this.nombre = nombre;
+            this.creditos = creditos;
+            this.docente = docente;
+        }
+
+        public string GetCodigo() => codigo;
+        public string GetNombre() => nombre;
+        public string GetCreditos() => creditos;
+        public string GetDocente() => docente;
+    }
+}
